ed67447d297aae3c611fbde4e20499707ecdd98c
Tyler <internetexplorerbad@robustjumprope.lol>
2025-11-25 14:52:28 -0500

    don't need .astro folder anymore

--- .astro/astro/actions.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "astro:actions" {
-	type Actions = typeof import("/Users/e202171406/Documents/dev/personal/js/chaos-control/src/actions")["server"];
-
-	export const actions: Actions;
-}
